0cc699d865b5e1c5d9c39c28d068b9a33f6fc664
Alexander Warren <84345382+alex-warren105@users.noreply.github.com>
2023-03-14 07:38:41 -0400

    Adjusted files for vite reacr app
    Converted original HTML into React component in App.jsx and removed old index.html
    
    Moved original CSS into App.css and removed old file
    
    Adjusted directory from previous commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <main>
+        <h1>Email Form</h1>
+        <form action="#" method="POST">
+          <label for="recipient-email">Recipient Email Address:</label>
+          <input type="email" id="recipient-email" name="recipient-email" required />
+
+          <label for="sender-contact">Sender Contact Information:</label>
+          <input type="text" id="sender-contact" name="sender-contact" required />
+
+          <label for="units-invoiced">Units Invoiced:</label>
+          <input type="number" id="units-invoiced" name="units-invoiced" required />
+
+          <label for="invoice-description">Invoice Description:</label>
+          <textarea id="invoice-description" name="invoice-description" required></textarea>
+
+          <input type="submit" value="Send!" />
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default App

6730810e12a1f0f207873b3dca2ac1d825204c7f
Alexander Warren <84345382+alex-warren105@users.noreply.github.com>
2023-03-14 17:08:24 -0400

    Reverted commits to get emailjs to work
    removed extra code from previous method that was not working and got emailjs to work with emailjs.sendForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+<<<<<<< Updated upstream
 import { send } from 'emailjs-com'
 import { useHistory } from 'react-router-dom'
+=======
+import emailjs from '@emailjs/browser'
+import React, { useRef } from 'react';
+>>>>>>> Stashed changes
 
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
-  const [toSend, setToSend] = useState({
-    recipientEmail: '',
-    senderContact: '',
-    unitsInvoiced: '',
-    invoiceDescription: '',
-  });
+const form = useRef();
 
+<<<<<<< Updated upstream
   const handleChange = (e) => {
     setToSend({ ...toSend, [e.target.name]: e.target.value });
   };
@@ -31,62 +31,48 @@ export default function App() {
         console.log('FAILED...', err);
       });
   }
+=======
+  const sendEmail = (e) => {
+    e.preventDefault();
+>>>>>>> Stashed changes
 
-  const showAlert = () => {
-    alert(
-      `Recipient Email Address: ${toSend.recipientEmail}\n` +
-      `Sender Contact Information: ${toSend.senderContact}\n` +
-      `Units Invoiced: ${toSend.unitsInvoiced}\n` +
-      `Invoice Description: ${toSend.invoiceDescription}`
-    );
-  }
+    emailjs.sendForm('default_service', 'template_efo3bp2', form.current, 'wFHLTcghtNMJbuPQZ')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
     <div className="App">
       <main>
         <h1>Email Form</h1>
-        <form action="#" method="POST">
+        <form ref={form} onSubmit={sendEmail}>
           <label htmlFor="recipientEmail">Recipient Email Address:</label>
           <input type="email" 
           id="recipientEmail" 
           name="recipientEmail"
-
-          value={toSend.recipientEmail}
-          onChange={handleChange}
-
           required />
 
           <label htmlFor="senderContact">Sender Contact Information:</label>
           <input type="text" 
           id="senderContact" 
           name="senderContact" 
-          
-          value={toSend.senderContact}
-          onChange={handleChange}
-          
           required />
 
           <label htmlFor="unitsInvoiced">Units Invoiced:</label>
           <input type="number" 
           id="unitsInvoiced" 
           name="unitsInvoiced" 
-          
-          value={toSend.unitsInvoiced}
-          onChange={handleChange}
-
           required />
 
           <label htmlFor="invoiceDescription">Invoice Description:</label>
           <textarea id="invoiceDescription" 
           name="invoiceDescription" 
-          
-          value={toSend.invoiceDescription}
-          onChange={handleChange}
-          
           required></textarea>
 
           <input type="submit" value="Send!" />
-          <button type="button" onClick={showAlert}>Display Form Values</button>
         </form>
       </main>
     </div>

9657ebe03db735ac06797440b2228decd1c80983
Alexander Warren <84345382+alex-warren105@users.noreply.github.com>
2023-03-14 12:56:04 -0400

    Emailjs implemented
    Added emailjs to send email from code
    
    changed form element IDs
    
    added toSend and handleChange functions to populate email from form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,91 @@
 import { useState } from 'react'
 import './App.css'
+import { send } from 'emailjs-com'
 
-function App() {
+
+export default function App() {
   const [count, setCount] = useState(0)
+
+  const [toSend, setToSend] = useState({
+    recipientEmail: '',
+    senderContact: '',
+    unitsInvoiced: '',
+    invoiceDescription: '',
+  });
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+  function onSubmit(e) {
+    e.preventDefault();
+    send(Bitvoice, template_efo3bp2, toSend, wFHLTcghtNMJbuPQZ)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  }
+
+  const showAlert = () => {
+    alert(
+      `Recipient Email Address: ${toSend.recipientEmail}\n` +
+      `Sender Contact Information: ${toSend.senderContact}\n` +
+      `Units Invoiced: ${toSend.unitsInvoiced}\n` +
+      `Invoice Description: ${toSend.invoiceDescription}`
+    );
+  }
 
   return (
     <div className="App">
       <main>
         <h1>Email Form</h1>
         <form action="#" method="POST">
-          <label for="recipient-email">Recipient Email Address:</label>
-          <input type="email" id="recipient-email" name="recipient-email" required />
+          <label htmlFor="recipientEmail">Recipient Email Address:</label>
+          <input type="email" 
+          id="recipientEmail" 
+          name="recipientEmail"
 
-          <label for="sender-contact">Sender Contact Information:</label>
-          <input type="text" id="sender-contact" name="sender-contact" required />
+          value={toSend.recipientEmail}
+          onChange={handleChange}
 
-          <label for="units-invoiced">Units Invoiced:</label>
-          <input type="number" id="units-invoiced" name="units-invoiced" required />
+          required />
 
-          <label for="invoice-description">Invoice Description:</label>
-          <textarea id="invoice-description" name="invoice-description" required></textarea>
+          <label htmlFor="senderContact">Sender Contact Information:</label>
+          <input type="text" 
+          id="senderContact" 
+          name="senderContact" 
+          
+          value={toSend.senderContact}
+          onChange={handleChange}
+          
+          required />
+
+          <label htmlFor="unitsInvoiced">Units Invoiced:</label>
+          <input type="number" 
+          id="unitsInvoiced" 
+          name="unitsInvoiced" 
+          
+          value={toSend.unitsInvoiced}
+          onChange={handleChange}
+
+          required />
+
+          <label htmlFor="invoiceDescription">Invoice Description:</label>
+          <textarea id="invoiceDescription" 
+          name="invoiceDescription" 
+          
+          value={toSend.invoiceDescription}
+          onChange={handleChange}
+          
+          required></textarea>
 
           <input type="submit" value="Send!" />
+          <button type="button" onClick={showAlert}>Display Form Values</button>
         </form>
       </main>
     </div>
   )
 }
 
-export default App

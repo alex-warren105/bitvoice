4eb702786aa84ba993f304b486dbcabd5dd32c88
Alexander Warren <84345382+alex-warren105@users.noreply.github.com>
2023-03-14 17:21:39 -0400

    Reverted commits to get emailjs to work
    removed extra code from previous method that was not working and got emailjs to work with emailjs.sendForm
    
    formatted form
    
    added select menu for currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-<<<<<<< Updated upstream
-import { send } from 'emailjs-com'
-import { useHistory } from 'react-router-dom'
-=======
 import emailjs from '@emailjs/browser'
 import React, { useRef } from 'react';
->>>>>>> Stashed changes
 
 
 export default function App() {
 
 const form = useRef();
 
-<<<<<<< Updated upstream
-  const handleChange = (e) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
-  };
-  
-  const history = useHistory();
-
-  function onSubmit(e) {
-    e.preventDefault();
-    send('Bitvoice', 'template_efo3bp2', toSend, 'wFHLTcghtNMJbuPQZ')
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        history.push('/receipt', { toSend });
-      })
-      .catch((err) => {
-        console.log('FAILED...', err);
-      });
-  }
-=======
   const sendEmail = (e) => {
     e.preventDefault();
->>>>>>> Stashed changes
 
     emailjs.sendForm('default_service', 'template_efo3bp2', form.current, 'wFHLTcghtNMJbuPQZ')
       .then((result) => {
@@ -50,25 +25,35 @@ const form = useRef();
         <h1>Email Form</h1>
         <form ref={form} onSubmit={sendEmail}>
           <label htmlFor="recipientEmail">Recipient Email Address:</label>
-          <input type="email" 
+          <input 
+          type="email" 
           id="recipientEmail" 
           name="recipientEmail"
           required />
 
           <label htmlFor="senderContact">Sender Contact Information:</label>
-          <input type="text" 
+          <input 
+          type="text" 
           id="senderContact" 
           name="senderContact" 
           required />
 
+          <label htmlFor="unitType">Currency</label>
+          <select name='unitType'>
+            <option>USD $</option>
+          </select>
+
+
           <label htmlFor="unitsInvoiced">Units Invoiced:</label>
-          <input type="number" 
+          <input 
+          type="number" 
           id="unitsInvoiced" 
           name="unitsInvoiced" 
           required />
 
           <label htmlFor="invoiceDescription">Invoice Description:</label>
-          <textarea id="invoiceDescription" 
+          <textarea 
+          id="invoiceDescription" 
           name="invoiceDescription" 
           required></textarea>
 

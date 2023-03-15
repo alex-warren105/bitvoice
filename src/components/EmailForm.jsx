2cd49fe45abfab602c31d5ed17206d1e5d3df532
Alexander Warren <84345382+alex-warren105@users.noreply.github.com>
2023-03-15 19:47:23 -0400

    Fixed another timestamp issue

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -2,58 +2,63 @@ import { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
 
 export default function EmailForm() {
-  const [timestamp, setTimestamp] = useState(null);
-  const form = useRef();
+    const [timestamp, setTimestamp] = useState('')
+    const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-    const now = new Date();
-    setTimestamp(now);
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log('Sending email...');
+        const now = new Date();
+        setTimestamp(now.toLocaleString());
+        console.log('Timestamp set to:', timestamp);
 
-    emailjs
-      .sendForm(
-        'default_service',
-        'template_efo3bp2',
-        form.current,
-        'wFHLTcghtNMJbuPQZ'
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+        const timestampField = form.current.elements.namedItem("timestamp");
+        timestampField.value = now.toLocaleString();
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label htmlFor="recipientEmail">Recipient Email Address:</label>
-      <input type="email" id="recipientEmail" name="recipientEmail" required />
+        emailjs
+        .sendForm(
+            'default_service',
+            'template_efo3bp2',
+            form.current,
+            'wFHLTcghtNMJbuPQZ'
+        )
+        .then(
+            (result) => {
+            console.log(result.text);
+            },
+            (error) => {
+            console.log(error.text);
+            }
+        );
+    };
 
-      <label htmlFor="senderContact">Sender Contact Information:</label>
-      <input type="text" id="senderContact" name="senderContact" required />
+    return (
+        <form ref={form} onSubmit={sendEmail}>
+        <label htmlFor="recipientEmail">Recipient Email Address:</label>
+        <input type="email" id="recipientEmail" name="recipientEmail" required />
 
-      <label htmlFor="unitType">Currency</label>
-      <select name="unitType">
-        <option>USD $</option>
-      </select>
+        <label htmlFor="senderContact">Sender Contact Information:</label>
+        <input type="text" id="senderContact" name="senderContact" required />
 
-      <label htmlFor="unitsInvoiced">Units Invoiced:</label>
-      <input type="number" id="unitsInvoiced" name="unitsInvoiced" required />
+        <label htmlFor="unitType">Currency</label>
+        <select name="unitType">
+            <option>USD $</option>
+        </select>
 
-      <label htmlFor="invoiceDescription">Invoice Description:</label>
-      <textarea id="invoiceDescription" name="invoiceDescription" required></textarea>
+        <label htmlFor="unitsInvoiced">Units Invoiced:</label>
+        <input type="number" id="unitsInvoiced" name="unitsInvoiced" required />
 
-      <input type="submit" value="Send!" />
+        <label htmlFor="invoiceDescription">Invoice Description:</label>
+        <textarea id="invoiceDescription" name="invoiceDescription" required></textarea>
 
-      <input
-        type="hidden"
-        id="timestamp"
-        name="timestamp"
-        value={timestamp ? timestamp.toLocaleString() : ''}
-      />
-    </form>
-  );
+        <input type="submit" value="Send!" />
+
+        <input 
+            type='hidden' 
+            id="timestamp" 
+            name='timestamp' 
+            value={timestamp}
+            />
+        </form>
+    );
 }
